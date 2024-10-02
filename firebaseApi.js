755cd157919e1e97160c59306b0343d00ff728f3
FacundoNeiman <facundo.neiman@davinci.edu.ar>
2024-10-02 10:35:01 -0300

    agregado de login y ver usuario logueado

--- firebaseApi.js
+++ firebaseApi.js
@@ -1,8 +1,32 @@
-import { registerUser } from './firebaseConfig.js';
+import { loginUser, registerUser, getUserLog } from './firebaseConfig.js';
+// import { onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
+
 
 const emailInput = document.querySelector('#email');
 const passInput = document.querySelector('#pass');
 const registerBtn = document.querySelector('#registerBtn');
+const loginBtn = document.querySelector('#loginBtn');
+const statusBtn = document.querySelector('#statusBtn');
+
+loginBtn.addEventListener('click', async () => {
+    const email = emailInput.value;
+    const password = passInput.value;
+
+    console.log(email, password)
+
+    // onAuthStateChanged(getUserLog(), (user) => {
+    //     if (user) {
+    //         console.log('Usuario ya logueado:', user);
+    //         return;
+    //     }
+    // });
+    try{
+        const user = await loginUser(email,password);
+        console.log(user);
+    } catch(error){
+        console.log( "Error", error);
+    }
+})
 
 registerBtn.addEventListener('click', async () => {
     const email = emailInput.value;
@@ -16,4 +40,10 @@ registerBtn.addEventListener('click', async () => {
     } catch (error) {
         console.error('Error en el registro:', error);
     }
+});
+
+
+
+statusBtn.addEventListener('click',  () => {
+    console.log(getUserLog())
 });
--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-analytics.js';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,  } from 'https://www.gstatic.com/firebasejs/10.14.0/firebase-auth.js';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -44,4 +44,9 @@ export async function registerUser(email, password) {
       console.error("Error en inicio de sesión:", error.message);
       throw error;
     }
+  }
+
+
+  export function getUserLog() {
+      return auth.currentUser.email;
   }
